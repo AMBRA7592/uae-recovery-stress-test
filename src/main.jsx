4326a1730f39e09232d7ebfca047e33d452205ce
ABrandes <79119643+AMBRA7592@users.noreply.github.com>
2026-03-22 22:29:02 +0100

    Add Vercel build scaffolding

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import UAEStressTest from '../model/uae_scenario_comparison.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <UAEStressTest />
+  </React.StrictMode>
+)
